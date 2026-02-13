97212f0f5c7347ea2405a3faf728250d29954288
Samudra <samudramitra2017@gmail.com>
2026-02-14 00:09:40 +0530

    added generalized code for segment tree

--- basic_segment_tree/Dynamic_Range_Sum_Queries.cpp
+++ basic_segment_tree/Dynamic_Range_Sum_Queries.cpp
@@ -1,5 +1,6 @@
 
 #include <bits/stdc++.h>
+#include "Generalized_Segment_Tree.h"
 using namespace std;
 typedef long long ll;
 typedef long double ld;
@@ -14,79 +15,6 @@ ll gcd(ll a, ll b)
 {
     return b == 0 ? a : gcd(b, a % b);
 }
-class SegmentTree
-{
-private:
-    vector<ll> arr;
-    vector<ll> tree;
-    ll null_value;
-    ll n;
-    ll combine(ll a, ll b)
-    {
-        return a + b;
-    }
-    void update_tree(ll idx, ll val, ll node, ll left, ll right)
-    {
-        if (left == right)
-        {
-            tree[node] = val;
-            return;
-        }
-        ll mid = (left + right) / 2;
-        if ((left <= idx) && (idx <= mid))
-        {
-            update_tree(idx, val, 2 * node + 1, left, mid);
-        }
-        else if (((mid + 1) <= idx) && (idx <= right))
-        {
-            update_tree(idx, val, 2 * node + 2, mid + 1, right);
-        }
-        tree[node] = combine(tree[2 * node + 1], tree[2 * node + 2]);
-    }
-    ll sum_tree(ll L, ll R, ll node, ll left, ll right)
-    {
-        if (L > R)
-            return null_value;
-        if ((left == L) && (right == R))
-        {
-            return tree[node];
-        }
-        ll mid = (left + right) / 2;
-        ll leftsum = sum_tree(max(L, left), min(mid, R), 2 * node + 1, left, mid);
-        ll rightsum = sum_tree(max(mid + 1, L), min(R, right), 2 * node + 2, mid + 1, right);
-        return combine(leftsum, rightsum);
-    }
-    void build(ll node, ll left, ll right)
-    {
-        if (left == right)
-        {
-            tree[node] = arr[left];
-            return;
-        }
-        ll mid = (left + right) / 2;
-        build(2 * node + 1, left, mid);
-        build(2 * node + 2, mid + 1, right);
-        tree[node] = combine(tree[2 * node + 1], tree[2 * node + 2]);
-    }
-
-public:
-    SegmentTree(vector<ll> v, ll nv)
-    {
-        arr = v;
-        n = arr.size();
-        null_value = nv;
-        tree.resize(4 * n, nv);
-        build(0, 0, n - 1);
-    }
-    void update(ll idx, ll val)
-    {
-        update_tree(idx, val, 0, 0, n - 1);
-    }
-    ll sum(ll L, ll R)
-    {
-        return sum_tree(L, R, 0, 0, n - 1);
-    }
-};
 int main()
 {
     fastio;
@@ -95,7 +23,7 @@ int main()
     vector<ll> v(n);
     for (auto &i : v)
         cin >> i;
-    SegmentTree st(v, 0);
+    SegmentTree<ll> st(v, 0);
     while (q--)
     {
         ll ty, a, b;
--- basic_segment_tree/Segment_tree.h
+++ basic_segment_tree/Segment_tree.h
@@ -1,38 +1,26 @@
 // Sarthak stalk krna buri baat hoti hai
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef long double ld;
-#define fastio                        \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
-#define max3(a, b, c) max(max(a, b), c)
-#define max4(a, b, c, d) max(max(a, b), max(c, d))
-#define fr(i, n) for (ll i = 0; i < n; i++)
-ll gcd(ll a, ll b)
-{
-    return b == 0 ? a : gcd(b, a % b);
-}
+
 class SegmentTree
 {
 private:
-    vector<ll> arr;
-    vector<ll> tree;
-    ll null_value;
-    ll n;
-    ll combine(ll a, ll b)
+    vector<long long> arr;
+    vector<long long> tree;
+    long long null_value;
+    long long n;
+    long long combine(long long a, long long b)
     {
         return a + b;
     }
-    void update_tree(ll idx, ll val, ll node, ll left, ll right)
+    void update_tree(long long idx, long long val, long long node, long long left, long long right)
     {
         if (left == right)
         {
             tree[node] = val;
             return;
         }
-        ll mid = (left + right) / 2;
+        long long mid = (left + right) / 2;
         if ((left <= idx) && (idx <= mid))
         {
             update_tree(idx, val, 2 * node + 1, left, mid);
@@ -43,7 +31,7 @@ private:
         }
         tree[node] = combine(tree[2 * node + 1], tree[2 * node + 2]);
     }
-    ll sum_tree(ll L, ll R, ll node, ll left, ll right)
+    long long sum_tree(long long L, long long R, long long node, long long left, long long right)
     {
         if (L > R)
             return null_value;
@@ -51,26 +39,26 @@ private:
         {
             return tree[node];
         }
-        ll mid = (left + right) / 2; // 0
-        ll leftsum = sum_tree(max(L, left), min(mid, R), 2 * node + 1, left, mid);
-        ll rightsum = sum_tree(max(mid + 1, L), min(R, right), 2 * node + 2, mid + 1, right);
+        long long mid = (left + right) / 2; // 0
+        long long leftsum = sum_tree(max(L, left), min(mid, R), 2 * node + 1, left, mid);
+        long long rightsum = sum_tree(max(mid + 1, L), min(R, right), 2 * node + 2, mid + 1, right);
         return combine(leftsum, rightsum);
     }
-    void build(ll node, ll left, ll right)
+    void build(long long node, long long left, long long right)
     {
         if (left == right)
         {
             tree[node] = arr[left];
             return;
         }
-        ll mid = (left + right) / 2;
+        long long mid = (left + right) / 2;
         build(2 * node + 1, left, mid);
         build(2 * node + 2, mid + 1, right);
         tree[node] = combine(tree[2 * node + 1], tree[2 * node + 2]);
     }
 
 public:
-    SegmentTree(vector<ll> v, ll nv)
+    SegmentTree(vector<long long> v, long long nv)
     {
         arr = v;
         n = arr.size();
@@ -78,20 +66,12 @@ public:
         tree.resize(4 * n, nv);
         build(0, 0, n - 1);
     }
-    void update(ll idx, ll val)
+    void update(long long idx, long long val)
     {
         update_tree(idx, val, 0, 0, n - 1);
     }
-    ll sum(ll L, ll R)
+    long long sum(long long L, long long R)
     {
         return sum_tree(L, R, 0, 0, n - 1);
     }
 };
-int main()
-{
-    fastio;
-    vector<ll> v = {1, 2, 3, 4};
-    SegmentTree st(v, 0);
-    st.update(0, 2);
-    cout << st.sum(0, 1) << "\n";
-}
